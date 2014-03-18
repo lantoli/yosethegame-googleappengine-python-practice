32fb395fa1aab0eb4c6e95461f5d564ca50607f2
Leo Antoli <leo.antoli@gmail.com>
2014-03-18 09:31:15 +0100

    example with flask template

--- flaskexample.py
+++ flaskexample.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/flask/hello')
-def hello():
-    return 'Hello World Flask!'
+app.debug = True
+
+@app.route('/flask/a')
+def helloa():
+    return 'returns: a'
+
+@app.route('/flask/b/')
+def hellob():
+    return 'returns: b/'
+
+@app.route('/flask/error')
+def errorA():
+    a = 1/0
+
+
+@app.route('/flask/hello/')
+@app.route('/flask/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return 'Sorry, Nothing at this URL.', 404
 
 @app.errorhandler(500)
-def page_not_found(e):
-    return 'Sorry, unexpected error: {}'.format(e), 500
+def page_error(e):
+    return 'Sorry, unexpected error', 200
